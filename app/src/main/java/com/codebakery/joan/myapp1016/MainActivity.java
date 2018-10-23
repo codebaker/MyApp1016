f7212d5da2ec84836c3b13c26193d85f96c7b9db
codebaker <jungah.kim76@gmail.com>
2018-10-23 13:36:36 +0900

    Initial commit

---
+++ app/src/main/java/com/codebakery/joan/myapp1016/MainActivity.java
@@ -0,0 +1,30 @@
+package com.codebakery.joan.myapp1016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtOnClicked(View view){
+        switch (view.getId())
+        {
+            case R.id.button1:
+                Toast.makeText(getApplicationContext(),"버튼1이 눌렸습니다.",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(getApplicationContext(),"버튼2이 눌렸습니다.",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(getApplicationContext(),"버튼3이 눌렸습니다.",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
